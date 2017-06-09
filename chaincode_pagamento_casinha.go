01e9087bd064a6e40b3ce10a4b29038676396662
Rodrigo Sclosa <rodrigo.sclosa@gmail.com>
2017-06-08 21:17:16 -0300

    Create chaincode_pagamento_casinha.go

---
+++ chaincode_pagamento_casinha.go
@@ -0,0 +1,205 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
+
+package main
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	pb "github.com/hyperledger/fabric/protos/peer"
+)
+
+// SimpleChaincode example simple Chaincode implementation
+type SimpleChaincode struct {
+}
+
+type Pagamento struct {
+	Pagador     string `json:"pagador"`
+	Recebedor   string `json:"recebedor"`
+	DataEntrada string `json:"dataEntrada"`
+	DataSaida   string `json:"dataSaida"`
+	Valor       int    `json:"valor"`
+}
+
+func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) pb.Response {
+	fmt.Printf("Inicializando Chaincode - Pagamento Casinha")
+
+	_, args := stub.GetFunctionAndParameters()
+	var Aval int
+	var err error
+
+	if len(args) != 1 {
+		return shim.Error("Incorrect number of arguments. Expecting 1")
+	}
+
+	// convert numeric string to integer
+	Aval, err = strconv.Atoi(args[0])
+	if err != nil {
+		return shim.Error("Expecting a numeric string argument to Init()")
+	}
+
+	// this is a very simple dumb test.  let's write to the ledger and error on any errors
+	err = stub.PutState("selftest", []byte(strconv.Itoa(Aval))) //making a test var "selftest", its handy to read this right away to test the network
+	if err != nil {
+		return shim.Error(err.Error())                          //self-test fail
+	}
+
+	fmt.Println("Pagamento Casinha - ready for action")                          //self-test pass
+	return shim.Success(nil)
+}
+
+// Transaction makes payment of X units from A to B
+func (t *SimpleChaincode) Pagar(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	var err error
+	fmt.Println("CHAMANDO Pagar")
+
+	if len(args) != 6 {
+		return shim.Error("Incorrect number of arguments. Expecting 6")
+	}
+
+	err = sanitize_arguments(args)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	Pagador := args[0]
+	Recebedor := args[1]
+	DataEntrada := args[3]
+	DataSaida := args[4]
+	Valor := args[5]
+
+	size, err := strconv.Atoi(args[5])
+	if err != nil {
+		return shim.Error("5rd argument must be a numeric string")
+	}
+
+	str := `{
+		"pagador": "` + Pagador + `",
+		"recebedor": "` + Recebedor + `", 
+		"dataEntrada": "` + DataEntrada + `", 
+		"dataSaida": ` + DataSaida + `, 
+		"valor": ` + strconv.Itoa(Valor) + `
+	}`
+
+	err = stub.PutState(Recebedor, []byte(str))
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	fmt.Println("- end Pagar")
+	return shim.Success(nil)
+}
+
+// Deletes an entity from state
+func (t *SimpleChaincode) delete(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	fmt.Printf("Running delete")
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 1")
+	}
+
+	Recebedor := args[0]
+
+	// Delete the key from the state in ledger
+	err := stub.DelState(Recebedor)
+	if err != nil {
+		return nil, errors.New("Failed to delete state")
+	}
+
+	return nil, nil
+}
+
+// Invoke callback representing the invocation of a chaincode
+// This chaincode will manage two accounts A and B and will transfer X units from A to B upon invoke
+func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Printf("Invoke called, determining function")
+
+	// Handle different functions
+	if function == "invoke" {
+		// Transaction makes payment of X units from A to B
+		fmt.Printf("Function is invoke")
+		return t.Pagar(stub, args)
+	} else if function == "init" {
+		fmt.Printf("Function is init")
+		return t.Init(stub, function, args)
+	} else if function == "delete" {
+		// Deletes an entity from its state
+		fmt.Printf("Function is delete")
+		return t.delete(stub, args)
+	}
+
+	return nil, errors.New("Received unknown function invocation")
+}
+
+func (t *SimpleChaincode) Run(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Printf("Run called, passing through to Invoke (same function)")
+
+	// Handle different functions
+	if function == "invoke" {
+		// Transaction makes payment of X units from A to B
+		fmt.Printf("Function is invoke")
+		return t.Pagar(stub, args)
+	} else if function == "init" {
+		fmt.Printf("Function is init")
+		return t.Init(stub, function, args)
+	} else if function == "delete" {
+		// Deletes an entity from its state
+		fmt.Printf("Function is delete")
+		return t.delete(stub, args)
+	}
+
+	return nil, errors.New("Received unknown function invocation")
+}
+
+// Query callback representing the query of a chaincode
+func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) pb.Response {
+	var key, jsonResp string
+	var err error
+	fmt.Println("CHAMANDO Query")
+
+	if len(args) != 1 {
+		return shim.Error("Incorrect number of arguments. Expecting key of the var to query")
+	}
+
+	// input sanitation
+	err = sanitize_arguments(args)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)           //get the var from ledger
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+		return shim.Error(jsonResp)
+	}
+
+	fmt.Println("- end Query")
+	return shim.Success(valAsbytes)                  //send it onward
+}
+
+func main() {
+	err := shim.Start(new(SimpleChaincode))
+	if err != nil {
+		fmt.Printf("Error starting Simple chaincode: %s", err)
+	}
+}
